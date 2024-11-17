5858972baa3bbb2115e3b9cec6c741bead4019e7
XeniaW <ksenia_donicheva@gmx.de>
2024-11-17 16:19:10 +0100

    user greetings

--- src/pages/UserView/training/Training.tsx
+++ src/pages/UserView/training/Training.tsx
@@ -1,40 +1,66 @@
-import { IonRow,IonCol, IonContent,IonButton,IonAlert, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React, { useState } from 'react';
+import {
+  IonRow,
+  IonCol,
+  IonContent,
+  IonButton,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import React from 'react';
+import { useAuth } from '../../../auth'; // Adjust the path to your AuthContext
 import './Training.css';
 
-
 const Training: React.FC = () => {
-  
+  const { email } = useAuth(); // Assuming `userEmail` is provided by AuthContext
 
   return (
-    <IonPage> 
-      <IonContent fullscreen>       
+    <IonPage>
+      <IonContent fullscreen>
         <IonHeader>
           <IonToolbar>
             <IonTitle size="large">Training</IonTitle>
           </IonToolbar>
         </IonHeader>
- 
-          <IonRow>
-            <IonCol className="ion-padding-top">
-            <img src="/assets/sil.png" alt={"MainImage"} />
-            </IonCol>    
-          </IonRow>
-          <IonRow>
-            <IonCol> 
-              <IonButton expand="full" color="light" routerLink="/my/trainingstart">
-              Start Training
-              </IonButton>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol><IonButton expand="full" color="light" routerLink="/my/sessions">View All Trainings</IonButton></IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol><IonButton expand="full" color="light" routerLink="/my/machines">Browse Machines</IonButton></IonCol>
-          </IonRow>
 
-        </IonContent>
+        {/* Welcome message */}
+        <IonRow>
+          <IonCol>
+            <h2 style={{ textAlign: 'center' }}>Hello {email || 'Guest'}!</h2>
+          </IonCol>
+        </IonRow>
+
+        {/* Main Image */}
+        <IonRow>
+          <IonCol className="ion-padding-top">
+            <img src="/assets/sil.png" alt="MainImage" />
+          </IonCol>
+        </IonRow>
+
+        {/* Buttons */}
+        <IonRow>
+          <IonCol>
+            <IonButton expand="full" color="light" routerLink="/my/trainingstart">
+              Start Training
+            </IonButton>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+            <IonButton expand="full" color="light" routerLink="/my/sessions">
+              View All Trainings
+            </IonButton>
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+            <IonButton expand="full" color="light" routerLink="/my/machines">
+              Browse Machines
+            </IonButton>
+          </IonCol>
+        </IonRow>
+      </IonContent>
     </IonPage>
   );
 };
