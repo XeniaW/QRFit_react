9c37bc375a8a33913bea06142a06477781daecd5
XeniaW <ksenia_donicheva@gmx.de>
2025-02-21 15:54:04 +0100

    added more muscles to 3d modell

--- src/data/muscles.ts
+++ src/data/muscles.ts
@@ -1,6 +1,6 @@
 export const MUSCLE_AREAS = [
   {
-    name: 'Pectoralis Major',
+    name: 'Chest',
     image: '/assets/muscles/pectoralis_major.jpg',
     xMin: -0.44,
     xMax: 0.46,
@@ -11,7 +11,7 @@ export const MUSCLE_AREAS = [
   },
   {
     /* Left */
-    name: 'Deltoid',
+    name: 'Shoulder',
     image: '/assets/muscles/deltoid_left.jpg',
     xMin: -1.02,
     xMax: -0.44,
@@ -23,7 +23,7 @@ export const MUSCLE_AREAS = [
 
   {
     /* Right */
-    name: 'Deltoid',
+    name: 'Shoulder',
     image: '/assets/muscles/deltoid_right.jpg',
     xMin: 0.55,
     xMax: 1.02,
@@ -80,23 +80,101 @@ export const MUSCLE_AREAS = [
   },
 
   {
-    name: 'Rectus Abdominis',
+    name: 'Abs',
     image: '/assets/muscles/abs.jpg',
     xMin: -0.3,
-    xMax: 0.3,
-    yMin: 1.0,
-    yMax: 1.3,
-    zMin: 0.1,
-    zMax: 0.3,
+    xMax: 0.26,
+    yMin: 0.52,
+    yMax: 1.1,
+    zMin: 0.12,
+    zMax: 0.23,
   },
   {
+    /* Left */
     name: 'Quadriceps',
     image: '/assets/muscles/quadriceps.jpg',
-    xMin: -0.5,
-    xMax: 0.5,
-    yMin: 0.5,
-    yMax: 1.0,
-    zMin: 0.1,
-    zMax: 0.4,
+    xMin: -0.6238,
+    xMax: -0.1465,
+    yMin: -0.8897,
+    yMax: 0.1578,
+    zMin: -0.2149,
+    zMax: 0.0847,
+  },
+  {
+    /* Right */
+    name: 'Quadriceps',
+    image: '/assets/muscles/quadriceps.jpg',
+    xMin: 0.1775,
+    xMax: 0.6526,
+    yMin: -0.9845,
+    yMax: 0.0499,
+    zMin: -0.2742,
+    zMax: 0.0463,
+  },
+  {
+    /* Left */
+    name: 'Hamstrings',
+    image: '/assets/muscles/hamstrings.jpg',
+    xMin: 0.0685,
+    xMax: 0.5278,
+    yMin: -1.0476,
+    yMax: -0.2415,
+    zMin: -0.524,
+    zMax: -0.4032,
+  },
+  {
+    /* Right */
+    name: 'Hamstrings',
+    image: '/assets/muscles/hamstrings.jpg',
+    xMin: -0.5635,
+    xMax: -0.0987,
+    yMin: -1.0775,
+    yMax: -0.0121,
+    zMin: -0.5927,
+    zMax: -0.364,
+  },
+  {
+    /* Left */
+    name: 'Glutes',
+    image: '/assets/muscles/glutes.jpg',
+    xMin: 0.0832,
+    xMax: 0.4866,
+    yMin: -0.0589,
+    yMax: 0.4541,
+    zMin: -0.6103,
+    zMax: -0.364,
+  },
+  {
+    /* Right */
+    name: 'Glutes',
+    image: '/assets/muscles/glutes.jpg',
+    xMin: -0.436,
+    xMax: -0.0548,
+    yMin: -0.0065,
+    yMax: 0.4488,
+    zMin: -0.6152,
+    zMax: -0.4387,
+  },
+  {
+    /* Left */
+    name: 'Calves',
+    image: '/assets/muscles/calves.jpg',
+    xMin: 0.3307,
+    xMax: 0.6557,
+    yMin: -2.4937,
+    yMax: -1.3001,
+    zMin: -0.5956,
+    zMax: -0.4363,
+  },
+  {
+    /* Right */
+    name: 'Calves',
+    image: '/assets/muscles/calves.jpg',
+    xMin: -0.6427,
+    xMax: -0.3169,
+    yMin: -2.6629,
+    yMax: -1.265,
+    zMin: -0.5932,
+    zMax: -0.4483,
   },
 ];
