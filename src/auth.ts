a3c3b8fdc2bb7df9f88a573d1f9a26fb59377106
XeniaW <ksenia_donicheva@gmx.de>
2025-04-28 16:42:16 +0200

    removed unused variables in auth + trying to bugfix the ts warnings

--- src/auth.ts
+++ src/auth.ts
@@ -1,17 +1,15 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
-import { onAuthStateChanged, User } from 'firebase/auth';
-import { auth } from './firebase';
+import React, { useContext } from 'react';
 
 export interface Auth {
-    loggedIn: boolean;
-    userId: string | null; // Allow null instead of undefined
-    email?: string | null; // Optional email
-  }
-  
-  export const AuthContext = React.createContext<Auth>({
-    loggedIn: false,
-    email: null,
-    userId: null, // Default to null
-  })
+  loggedIn: boolean;
+  userId: string | null; // Allow null instead of undefined
+  email?: string | null; // Optional email
+}
+
+export const AuthContext = React.createContext<Auth>({
+  loggedIn: false,
+  email: null,
+  userId: null, // Default to null
+});
 
 export const useAuth = () => useContext(AuthContext);
